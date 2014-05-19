8309db9c072b5865ddee06021e9b8e275744e29e
Yohei YASUKAWA <yasulab@gmail.com>
2014-05-19 22:00:57 +0900

    Design layout of code (cf. The Art of Readable Code)

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 require "rubygems"
 require "bundler/setup"
 
 ## -- Config -- ##
 
-posts_dir       = "_posts"    # directory for blog files
-new_post_ext    = "md"  # default new post file extension when using the new_post task
-new_page_ext    = "md"  # default new page file extension when using the new_page task
+posts_dir       = "_posts" # directory for blog files
+new_post_ext    = "md"     # default new post file extension when using the new_post task
+new_page_ext    = "md"     # default new page file extension when using the new_page task
 
 
 #############################
@@ -16,12 +17,12 @@ new_page_ext    = "md"  # default new page file extension when using the new_pag
 desc "Create a new post in #{posts_dir}"
 task :new_post do |t, args|
   #タイトルは自動で作ることにする。
-  display_name = get_stdin('あなたの名前は?: ')
-  privious_date = process[:date].strftime('%Y-%m-%d')
+  display_name   = get_stdin('あなたの名前は?: ')
+  privious_date  = process[:date].strftime('%Y-%m-%d')
   privious_times = process[:times]
 
   times = get_stdin("何回目のOkinawarb Meetupsに参加しましたか?(前回開催(#{privious_date}):#{privious_times}回): ")
-  times = process[:times] if times == ''
+  times = process[:times] if times.empty?
 
   git_checkout("#{Time.now.strftime('%Y-%m-%d')}-no#{times}-#{display_name}")
 
@@ -39,15 +40,20 @@ task :new_post do |t, args|
     post.puts "## #{display_name}"
     post.puts "## やること宣言"
     post.puts "※[Okinawarb/meetups のissuesで書いたやること宣言](https://github.com/okinawarb/meetups/issues)のリンクをはってみましょう。"
+    post.puts ""
     post.puts "## やったこと"
     post.puts "※やったことをかいていきましょう。"
+    post.puts ""
     post.puts "## 振り返り"
     post.puts "### 良かったこと"
     post.puts "※今回の活動でよかったことを教えて下さい"
+    post.puts ""
     post.puts "### ここは直した方がいいなぁと思ったこと"
     post.puts "※今回の活動でここは直したいなぁ、工夫したいなぁと思ったことを教えて下さい"
+    post.puts ""
     post.puts "### 次回改善したいこと"
     post.puts "※次回参加するなら、こうしたい、試してみたいことがあれば共有しましょう！"
+    post.puts ""
   end
 end
 
@@ -58,12 +64,12 @@ end
 
 def process
   process = (Time.now - Time.new(2014,5,21)).div(24*60*60)
-  if process < 0
+  if    process < 0
     times = 117
-    date = Time.new(2014,5,21)
+    date  = Time.new(2014,5,21)
   elsif process > 0
     times = 117 + process.div(7)
-    date = Time.new(2014,5,21) + 7 * process.div(7) * 24 * 60 * 60
+    date  = Time.new(2014,5,21) + 7 * process.div(7) * 24 * 60 * 60
   end
   {times:times, date: date }
 end
